2b16c07fa2669ccda5c9e905ce9848e241aa8f69
Robert Huselius <robert@huseli.us>
2020-06-30 21:52:52 +0200

    Remedy import error on ./setup install

--- spr/__init__.py
+++ spr/__init__.py
@@ -1,6 +1,8 @@
-from spr.config import Config
-from spr.engine import SPR
-
+try:
+    from spr.config import Config
+    from spr.engine import SPR
+except ImportError:
+    pass
 
 __version__ = "0.3.0"
 VERSION = tuple(map(int, __version__.split(".")))
